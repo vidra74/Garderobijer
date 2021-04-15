1ce72b4deb5db9ef55636e2d4aefab24d36b9303
fjancic@inet.hr <vidra74>
2021-04-15 07:48:37 +0200

    Glavni form organiziran kao tabbed application

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Garderobijer
 Aplikacija za voðenje vlastite garderobe: velièine, sadržaj ormara, kupovine, TODO lista, fotografije, cijene.
 
-7.4.2021
+14.4.2021
+Napravio Tabbed glavni form kao okvir aplikacije.
+
+08.4.2021
+Sreðivanje izgleda poèetnog ekrana. Prebacivanje Android projektnih datoteka.
+
+07.4.2021
 Dodana podrška za ini settings datoteku. Koristim CCR.PrefsIniFile unite sa
 https://delphihaven.wordpress.com/2015/06/11/ccr-prefsinifile-on-github/
-
-8.4.2021
-Sreðivanje izgleda poèetnog ekrana. Prebacivanje Android projektnih datoteka.
--- V1/untFrm.pas
+++ V1/untFrm.pas
@@ -4,24 +4,34 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit;
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.TabControl,
+  FMX.StdCtrls, FMX.Gestures, FMX.Controls.Presentation, FMX.Edit;
 
 type
   TFrmMain = class(TForm)
-    lblNaslov: TLabel;
+    HeaderToolBar: TToolBar;
+    ToolBarLabel: TLabel;
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
+    TabItem3: TTabItem;
+    TabItem4: TTabItem;
+    GestureManager1: TGestureManager;
     lblBrojPokretanja: TLabel;
     edtBrojPokretanja: TEdit;
-    sbZatvori: TSpeedButton;
-    procedure btnZatvoriClick(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormGesture(Sender: TObject; const EventInfo: TGestureEventInfo;
+      var Handled: Boolean);
     procedure FormShow(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { Private declarations }
     FBrojPokretanja: Integer;
+    FRebatinke: String;
   public
     { Public declarations }
     property BrojPokretanja: Integer read FBrojPokretanja write FBrojPokretanja;
+    property Rebatinke: String read FRebatinke write FRebatinke;
   end;
 
 var
@@ -34,11 +44,11 @@ implementation
 uses System.IniFiles
   , CCR.PrefsIniFile;
 
-procedure TFrmMain.btnZatvoriClick(Sender: TObject);
+procedure TFrmMain.FormCreate(Sender: TObject);
 begin
-  Close;
+  { This defines the default active tab at runtime }
+  TabControl1.ActiveTab := TabItem1;
 end;
-
 
 procedure TFrmMain.FormDestroy(Sender: TObject);
 var
@@ -49,9 +59,33 @@ begin
   try
     Broj := edtBrojPokretanja.Text.ToInteger + 1;
     Settings.WriteInteger('General', 'BrojPokretanja', Broj);
+    Settings.WriteString('Velièine', 'Rebatinke', Rebatinke);
   finally
     Settings.Free;
   end;
+
+end;
+
+procedure TFrmMain.FormGesture(Sender: TObject;
+  const EventInfo: TGestureEventInfo; var Handled: Boolean);
+begin
+{$IFDEF ANDROID}
+  case EventInfo.GestureID of
+    sgiLeft:
+    begin
+      if TabControl1.ActiveTab <> TabControl1.Tabs[TabControl1.TabCount-1] then
+        TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex+1];
+      Handled := True;
+    end;
+
+    sgiRight:
+    begin
+      if TabControl1.ActiveTab <> TabControl1.Tabs[0] then
+        TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex-1];
+      Handled := True;
+    end;
+  end;
+{$ENDIF}
 end;
 
 procedure TFrmMain.FormShow(Sender: TObject);
@@ -62,10 +96,12 @@ begin
   Settings := CreateUserPreferencesIniFile;
   try
     Broj := Settings.ReadInteger('General', 'BrojPokretanja', 0);
+    Rebatinke := Settings.ReadString('Velièine', 'Rebatinke', '54');
     edtBrojPokretanja.Text := Broj.ToString;
   finally
     Settings.Free;
   end;
+
 end;
 
 end.
